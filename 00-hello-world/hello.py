548e594a0ba9b4d72d0ff5cdba28fc1c75c533ab
Lars De Richter <lars.derichter@gmail.com>
2025-01-28 10:02:54 +0100

    Hello world

---
+++ 00-hello-world/hello.py
@@ -0,0 +1,9 @@
+# Dit is mijn eerste Python programma
+print("Hello IoT World!")
+
+# Laten we wat berekeningen doen
+print(2 + 3)
+
+# We kunnen ook tekst en getallen combineren
+aantal_sensors = 5
+print("Mijn IoT project gebruikt", aantal_sensors, "sensors")
